1dfe49550b02e2838f1ab535c6ca243ff78f2ef1
code-beans <marvin.neurath@code-beans.com>
2018-05-19 22:42:05 +0200

    adds demo project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # GizmoExtensions
 Utility class(es) that extend Unity's Gizmos
+
+![](gizmos.png)
+
+
+## Install
+Just drop the `/src` folder into your Unity Assets
---
+++ demo/GizmoExtensions/Assets/demo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Utils;
+
+[ExecuteInEditMode]
+public class demo : MonoBehaviour {
+
+    private float timePassed = 0;
+    private bool toMax = true;
+    public float degreesPerSecond = 20;
+	// Update is called once per frame
+	void OnDrawGizmos () {
+
+	    if (toMax) {
+	        timePassed += Time.deltaTime;
+	        if (timePassed >= 1)
+	            toMax = !toMax;
+	    }
+	    else {
+	        timePassed -= Time.deltaTime;
+	        if (timePassed <= 0)
+	            toMax = true;
+	    }
+
+	    var rot = Quaternion.AngleAxis((Time.time * degreesPerSecond) % 360, Vector3.up);
+        Gizmos.color = Color.Lerp(Color.red, Color.green, timePassed);
+		GizmosExtensions.DrawWireCube(Vector3.zero, Vector3.one, rot);
+	    GizmosExtensions.DrawWireSphere(Vector3.forward * 2,0.5f,rot);
+        GizmosExtensions.DrawArrow(Vector3.forward, Vector3.forward + Vector3.up * 2);
+    }
+}
