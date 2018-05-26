a90edca9191c4a52096a34696ce3ad1060ffd353
code-beans <marvin.neurath@code-beans.com>
2018-05-26 23:18:39 +0200

    add bugfixes \n capsule as primitive type\n removes gif from repo

--- README.md
+++ README.md
@@ -1,15 +1,20 @@
 # GizmoExtensions
 Utility class(es) that extend Unity's Gizmos
 
-![](gif.gif)
+![](http://code-beans.com/static/gif.gif)
 
 ## Features
 
 * adds `Quaternion rotation` as an input parameter for the primitive types
-* adds Circles, Cylinders, Arcs and Arrows to the Gizmos
+* Adds the following Gizmos:
+  * Circles
+  * Cylinders
+  * Arcs
+  * Arrows
+  * Capsules
 
 ## Install
 Just drop the `/src` folder into your Unity Assets
 
 ## Contribute
-Do you have any Gizmo extensions? Feel free to contribute !
+Do you have any Gizmo extensions? Feel free to contribute!
--- src/GizmosExtensions.cs
+++ src/GizmosExtensions.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 namespace Utils {
 	public static class GizmosExtensions {
 	   
+		/// <summary>
+		/// Draws a wire cube with a given rotation 
+		/// </summary>
+		/// <param name="center"></param>
+		/// <param name="size"></param>
+		/// <param name="rotation"></param>
 		public static void DrawWireCube(Vector3 center, Vector3 size, Quaternion rotation = default(Quaternion)) {
 			var old = Gizmos.matrix;
+			if (rotation.Equals(default(Quaternion)))
+				rotation = Quaternion.identity;
 			Gizmos.matrix = Matrix4x4.TRS(center, rotation, size);
 			Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
 			Gizmos.matrix = old;
@@ -22,6 +30,8 @@ namespace Utils {
 
 		public static void DrawWireSphere(Vector3 center, float radius, Quaternion rotation = default(Quaternion)) {
 			var old = Gizmos.matrix;
+			if (rotation.Equals(default(Quaternion)))
+				rotation = Quaternion.identity;
 			Gizmos.matrix = Matrix4x4.TRS(center, rotation, Vector3.one);
 			Gizmos.DrawWireSphere(Vector3.zero, radius);
 			Gizmos.matrix = old;
@@ -29,7 +39,7 @@ namespace Utils {
 
 		
 		/// <summary>
-		/// Draws a circle with normal (up)
+		/// Draws a flat wire circle (up)
 		/// </summary>
 		/// <param name="center"></param>
 		/// <param name="radius"></param>
@@ -40,24 +50,53 @@ namespace Utils {
 		}
 
 	    /// <summary>
-	    /// Draws an arc
+	    /// Draws an arc with a rotation around the center
 	    /// </summary>
-	    /// <param name="center"></param>
-	    /// <param name="radius"></param>
+	    /// <param name="center">center point</param>
+	    /// <param name="radius">radiu</param>
 	    /// <param name="angle">angle in degrees</param>
-	    /// <param name="segments"></param>
-	    /// <param name="rotation"></param>
-	    public static void DrawWireArc(Vector3 center, float radius, float angle, int segments = 20,Quaternion rotation = default(Quaternion))
+	    /// <param name="segments">number of segments</param>
+	    /// <param name="rotation">rotation around the center</param>
+	    public static void DrawWireArc(Vector3 center, float radius, float angle, int segments = 20, Quaternion rotation = default(Quaternion))
 		{
+			
 			var old = Gizmos.matrix;
-			Gizmos.matrix = Matrix4x4.Rotate(rotation);
-			Vector3 from = center + Vector3.forward * radius;
-			Vector3 to = center;
+		
+			Gizmos.matrix = Matrix4x4.TRS(center,rotation,Vector3.one);
+			Vector3 from = Vector3.forward * radius;
+			var step = Mathf.RoundToInt(angle / segments);
+			for (int i = 0; i <= angle; i += step) {
+				var to = new Vector3(radius * Mathf.Sin(i * Mathf.Deg2Rad), 0, radius * Mathf.Cos(i * Mathf.Deg2Rad));
+				Gizmos.DrawLine(from, to);
+				from = to;
+			}
+
+			Gizmos.matrix = old;
+		}
+
+
+		/// <summary>
+		/// Draws an arc with a rotation around an arbitraty center of rotation
+		/// </summary>
+		/// <param name="center">the circle's center point</param>
+		/// <param name="radius">radius</param>
+		/// <param name="angle">angle in degrees</param>
+		/// <param name="segments">number of segments</param>
+		/// <param name="rotation">rotation around the centerOfRotation</param>
+		/// <param name="centerOfRotation">center of rotation</param>
+		public static void DrawWireArc(Vector3 center, float radius, float angle, int segments, Quaternion rotation, Vector3 centerOfRotation)
+		{
+
+			var old = Gizmos.matrix;
+			if (rotation.Equals(default(Quaternion)))
+				rotation = Quaternion.identity;
+			Gizmos.matrix = Matrix4x4.TRS(centerOfRotation, rotation, Vector3.one);
+			var deltaTranslation = centerOfRotation - center;
+			Vector3 from = deltaTranslation + Vector3.forward * radius;
 			var step = Mathf.RoundToInt(angle / segments);
 			for (int i = 0; i <= angle; i += step)
 			{
-				to.x = center.x + radius * Mathf.Sin(i * Mathf.Deg2Rad);
-				to.z = center.z + radius * Mathf.Cos(i * Mathf.Deg2Rad);
+				var to = new Vector3(radius * Mathf.Sin(i * Mathf.Deg2Rad), 0, radius * Mathf.Cos(i * Mathf.Deg2Rad)) + deltaTranslation;
 				Gizmos.DrawLine(from, to);
 				from = to;
 			}
@@ -66,7 +105,30 @@ namespace Utils {
 		}
 
 		/// <summary>
-		/// Draws a wire cylinder face up
+		/// Draws an arc with a rotation around an arbitraty center of rotation
+		/// </summary>
+		/// <param name="matrix">Gizmo matrix applied before drawing</param>
+		/// <param name="radius">radius</param>
+		/// <param name="angle">angle in degrees</param>
+		/// <param name="segments">number of segments</param>
+		public static void DrawWireArc(Matrix4x4 matrix, float radius, float angle, int segments)
+		{
+			var old = Gizmos.matrix;
+			Gizmos.matrix = matrix;
+			Vector3 from = Vector3.forward * radius;
+			var step = Mathf.RoundToInt(angle / segments);
+			for (int i = 0; i <= angle; i += step)
+			{
+				var to = new Vector3(radius * Mathf.Sin(i * Mathf.Deg2Rad), 0, radius * Mathf.Cos(i * Mathf.Deg2Rad));
+				Gizmos.DrawLine(from, to);
+				from = to;
+			}
+
+			Gizmos.matrix = old;
+		}
+
+		/// <summary>
+		/// Draws a wire cylinder face up with a rotation around the center
 		/// </summary>
 		/// <param name="center"></param>
 		/// <param name="radius"></param>
@@ -74,14 +136,54 @@ namespace Utils {
 		/// <param name="rotation"></param>
 		public static void DrawWireCylinder(Vector3 center, float radius, float height, Quaternion rotation = default(Quaternion)) {
 			var old = Gizmos.matrix;
-			Gizmos.matrix = Matrix4x4.Rotate(rotation);
-			var half = height / 2f;
-			DrawWireCircle(center - Vector3.up * half,radius);
-			DrawWireCircle(center + Vector3.up * half, radius);
-			Gizmos.DrawLine(center + Vector3.right * radius - Vector3.up * half, center + Vector3.right * radius + Vector3.up * half);
-			Gizmos.DrawLine(center - Vector3.right * radius - Vector3.up * half, center - Vector3.right * radius + Vector3.up * half);
-			Gizmos.DrawLine(center + Vector3.forward * radius - Vector3.up * half, center + Vector3.forward * radius + Vector3.up * half);
-			Gizmos.DrawLine(center - Vector3.forward * radius - Vector3.up * half, center - Vector3.forward * radius + Vector3.up * half);
+			if (rotation.Equals(default(Quaternion)))
+				rotation = Quaternion.identity;
+			Gizmos.matrix = Matrix4x4.TRS(center,rotation,Vector3.one);
+			var half = height / 2;
+			
+			//draw the 4 outer lines
+			Gizmos.DrawLine( Vector3.right * radius - Vector3.up * half,  Vector3.right * radius + Vector3.up * half);
+			Gizmos.DrawLine( - Vector3.right * radius - Vector3.up * half,  -Vector3.right * radius + Vector3.up * half);
+			Gizmos.DrawLine( Vector3.forward * radius - Vector3.up * half,  Vector3.forward * radius + Vector3.up * half);
+			Gizmos.DrawLine( - Vector3.forward * radius - Vector3.up * half,  - Vector3.forward * radius + Vector3.up * half);
+
+			//draw the 2 cricles with the center of rotation being the center of the cylinder, not the center of the circle itself
+			DrawWireArc(center + Vector3.up * half,radius,360,20,rotation, center);
+			DrawWireArc(center + Vector3.down * half, radius, 360, 20, rotation, center);
+			Gizmos.matrix = old;
+		}
+
+		/// <summary>
+		/// Draws a wire capsule face up
+		/// </summary>
+		/// <param name="center"></param>
+		/// <param name="radius"></param>
+		/// <param name="height"></param>
+		/// <param name="rotation"></param>
+		public static void DrawWireCapsule(Vector3 center, float radius, float height, Quaternion rotation = default(Quaternion))
+		{
+			if (rotation.Equals(default(Quaternion)))
+				rotation = Quaternion.identity;
+			var old = Gizmos.matrix;
+			Gizmos.matrix = Matrix4x4.TRS(center,rotation,Vector3.one);
+			var half = height / 2 - radius;
+		
+			//draw cylinder base
+			DrawWireCylinder(center,radius,height - radius * 2,rotation);
+
+			//draw upper cap
+			//do some cool stuff with orthogonal matrices
+			var mat = Matrix4x4.Translate(center + rotation * Vector3.up * half) * Matrix4x4.Rotate(rotation * Quaternion.AngleAxis(90,Vector3.forward));
+			DrawWireArc(mat,radius,180,20);
+			mat = Matrix4x4.Translate(center + rotation * Vector3.up * half) * Matrix4x4.Rotate(rotation * Quaternion.AngleAxis(90,Vector3.up)* Quaternion.AngleAxis(90, Vector3.forward));
+			DrawWireArc(mat, radius, 180, 20);
+			
+			//draw lower cap
+			mat = Matrix4x4.Translate(center + rotation * Vector3.down * half) * Matrix4x4.Rotate(rotation * Quaternion.AngleAxis(90, Vector3.up) * Quaternion.AngleAxis(-90, Vector3.forward));
+			DrawWireArc(mat, radius, 180, 20);
+			mat = Matrix4x4.Translate(center + rotation * Vector3.down * half) * Matrix4x4.Rotate(rotation * Quaternion.AngleAxis(-90, Vector3.forward));
+			DrawWireArc(mat, radius, 180, 20);
+		
 			Gizmos.matrix = old;
 
 		}
