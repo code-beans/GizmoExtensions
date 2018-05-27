b30212bcc99ae68655436b6e698f7ab283ea31b8
code-beans <marvin.neurath@code-beans.com>
2018-05-27 16:03:57 +0200

    demo project

---
+++ demo/Demo/Assets/Demo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+
+[ExecuteInEditMode]
+public class Demo : MonoBehaviour {
+	public Color color = Color.magenta;
+	public GizmoType Type = GizmoType.Cube;
+
+	public float Height =2;
+	public float Radius = 0.5f;
+	public float Degrees =45;
+
+
+	// Update is called once per frame
+	void OnDrawGizmos () {
+		Gizmos.color = color;
+		switch (Type) {
+			case GizmoType.Capsule:
+				GizmosExtensions.DrawWireCapsule(transform.position,Radius, Height, transform.rotation);
+				break;
+			case GizmoType.Circle:
+				GizmosExtensions.DrawWireCircle(transform.position,Radius,60,transform.rotation);
+				break;
+			case GizmoType.Arrow:
+				GizmosExtensions.DrawArrow(transform.position, transform.position + transform.rotation * (Vector3.up * Height));
+				break;
+			case GizmoType.Arc:
+				GizmosExtensions.DrawWireArc(transform.position,Radius,Degrees,20,transform.rotation);
+				break;
+			case GizmoType.Cube:
+				GizmosExtensions.DrawWireCube(transform.position,transform.localScale,transform.rotation);
+				break;
+			case GizmoType.Cylinder:
+				GizmosExtensions.DrawWireCylinder(transform.position,Radius,Height, transform.rotation);
+				break;
+			case GizmoType.Sphere:
+				GizmosExtensions.DrawWireSphere(transform.position,Radius,transform.rotation);
+				break;
+			default:
+				throw new ArgumentOutOfRangeException();
+		}
+	}
+
+	public enum GizmoType {
+		Capsule,Circle, Arrow, Arc, Cube, Cylinder, Sphere
+	}
+}
---
+++ demo/Demo/Assets/VectorField.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+
+[ExecuteInEditMode]
+public class VectorField : MonoBehaviour {
+
+	
+	public float mass = 1;
+
+
+
+	public int rows = 10, cols = 10;
+	
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.black;
+
+		
+			var target = transform.position;
+			target.y = 0;
+
+
+		for (int x = 0; x < rows; x++) {
+			for (int y = 0; y < cols; y++) {
+				var from = new Vector3(x,0,y);
+				var direction = target - from;
+				var to = from + direction.normalized * Mathf.Min(0.5f,mass/direction.sqrMagnitude);
+				
+				GizmosExtensions.DrawArrow(from,to);
+			}
+		}
+		
+	}
+}
